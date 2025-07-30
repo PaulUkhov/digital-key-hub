b199fbf8f72b2ba4e61e2e9cd77d1cd12231d090
Bogdan Pryadko <Bogdanpryadko1@gmail.com>
2025-07-30 13:44:27 +0300

    feat: add storage-service logic with minio

---
+++ digitalkeyhub-storage/src/main/java/com/audio/service/StorageService.java
@@ -0,0 +1,72 @@
+package com.audio.service;
+
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.RemoveObjectArgs;
+import io.minio.GetObjectArgs;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+
+@Service
+public class StorageService {
+    private final MinioClient minioClient;
+    private final String bucketName;
+
+    public StorageService(
+            @Value("${minio.endpoint}") String endpoint,
+            @Value("${minio.access-key}") String accessKey,
+            @Value("${minio.secret-key}") String secretKey,
+            @Value("${minio.bucket-name}") String bucketName) throws Exception {
+
+        this.minioClient = MinioClient.builder()
+                .endpoint(endpoint)
+                .credentials(accessKey, secretKey)
+                .build();
+        this.bucketName = bucketName;
+
+        initBucket();
+    }
+
+    private void initBucket() throws Exception {
+        boolean found = minioClient.bucketExists(BucketExistsArgs.builder()
+                .bucket(bucketName)
+                .build());
+
+        if (!found) {
+            minioClient.makeBucket(MakeBucketArgs.builder()
+                    .bucket(bucketName)
+                    .build());
+        }
+    }
+
+    public String uploadFile(MultipartFile file, String objectName) throws Exception {
+        try (InputStream inputStream = file.getInputStream()) {
+            minioClient.putObject(PutObjectArgs.builder()
+                    .bucket(bucketName)
+                    .object(objectName)
+                    .stream(inputStream, file.getSize(), -1)
+                    .contentType(file.getContentType())
+                    .build());
+            return objectName;
+        }
+    }
+
+    public void deleteFile(String objectName) throws Exception {
+        minioClient.removeObject(RemoveObjectArgs.builder()
+                .bucket(bucketName)
+                .object(objectName)
+                .build());
+    }
+
+    public InputStream getFile(String objectName) throws Exception {
+        return minioClient.getObject(GetObjectArgs.builder()
+                .bucket(bucketName)
+                .object(objectName)
+                .build());
+    }
+}
